9cf4dc9ae6c98a99411932acaefe09142e7fd294
Albertukraine <101990726+Albertukraine@users.noreply.github.com>
2023-01-26 21:31:35 +0200

    add checkups joi on body put and post

--- models/contacts.js
+++ models/contacts.js
@@ -33,13 +33,13 @@ async function removeContact(contactId) {
   const indexOfContact = contactList.findIndex(
     (contact) => contact.id === contactId
   );
-  console.log('indexOfContact',indexOfContact);
+  console.log("indexOfContact", indexOfContact);
   await contactList.splice(indexOfContact, 1);
   await fs.writeFile(contactsPath, JSON.stringify(contactList));
   return contactList;
 }
 
-async function updateContact (contactId, body) {
+async function updateContact(contactId, body) {
   const data = await fs.readFile(contactsPath, "utf8");
   const contactList = JSON.parse(data);
   const [contactToUpdate] = contactList.filter((item) => item.id === contactId);
@@ -48,7 +48,7 @@ async function updateContact (contactId, body) {
   contactToUpdate.phone = body.phone;
   await fs.writeFile(contactsPath, JSON.stringify(contactList), "utf8");
   return contactList;
-};
+}
 
 module.exports = {
   listContacts,
@@ -58,4 +58,4 @@ module.exports = {
   updateContact,
 };
 
-// 
+//
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -89,6 +89,11 @@ router.delete("/:contactId", async (req, res, next) => {
 
 router.put("/:contactId", async (req, res, next) => {
   try {
+    const { error } = contactSchema.validate(req.body);
+    if (error) {
+      error.status = 400;
+      throw error;
+    };
     const id = req.params.contactId;
     const result = await getContactById(id);
     if (result.length < 1) { res.status(404).json({
@@ -96,14 +101,23 @@ router.put("/:contactId", async (req, res, next) => {
       code: 404,
       message: `Not found contact with ${id}`
     });
-  return}
+  return};
 
   await updateContact(id, req.body);
+  res.status(200).json({
+    status: "succes",
+    code: 200,
+    data: {
+      result,
+    },
+  });
     
   } catch (error) {
+    next(error);
+    
     
   }
-  res.json({ message: "template message" });
+  
 });
 
 module.exports = router;
