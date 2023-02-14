5195fb230d4fd3f0e23b1889407200ee52cc0d04
Albertukraine <101990726+Albertukraine@users.noreply.github.com>
2023-02-15 01:45:34 +0200

    delete console log

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,31 +1,29 @@
-const User = require('../models/user');
-const {Unauthorized} = require('http-errors');
-const jwt = require('jsonwebtoken');
-const {SECRET_KEY} = process.env;
+const User = require("../models/user");
+const { Unauthorized } = require("http-errors");
+const jwt = require("jsonwebtoken");
+const { SECRET_KEY } = process.env;
 
-const auth = async(req, res, next) => {
-    console.log("HEADERS",req.header);
-    const { authorization = "" } = req.headers;
-    const [bearer, token] = authorization.split(" ");
-   
-    try {
-        if (bearer !== "Bearer") {throw new Unauthorized("Not authorized")};
-        const {id} = jwt.verify(token, SECRET_KEY);
-        const user = await User.findById(id);
-        if(!user || !user.token) { throw new Unauthorized("Not authorized")};
-        req.user = user;
-        next();
-    } catch (error) {
-        if (error.message === "Invalid signature") { error.status = 401;
-        }
-        next(error);
-        
+const auth = async (req, res, next) => {
+  const { authorization = "" } = req.headers;
+  const [bearer, token] = authorization.split(" ");
+
+  try {
+    if (bearer !== "Bearer") {
+      throw new Unauthorized("Not authorized");
     }
-
-
-}
-
+    const { id } = jwt.verify(token, SECRET_KEY);
+    const user = await User.findById(id);
+    if (!user || !user.token) {
+      throw new Unauthorized("Not authorized");
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    if (error.message === "Invalid signature") {
+      error.status = 401;
+    }
+    next(error);
+  }
+};
 
 module.exports = auth;
-
-
