9ecfb7510770dbefbc5076258bed5416728f01a3
Albertukraine <101990726+Albertukraine@users.noreply.github.com>
2023-02-22 00:39:45 +0200

    add avatar logics

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,13 +1,15 @@
 const User = require("../../models/user");
 const {Conflict} = require('http-errors');
 const bcrypt = require('bcrypt');
+const gravatar = require('gravatar');
 
 const register = async (req, res) => {
   const { name, email, password } = req.body;
   const user = await User.findOne({ email });
   const hashPass = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
   if (!user) {
-    await User.create({ name, email, password: hashPass });
+    const avatarURL = gravatar.url(email);
+    await User.create({ name, email, avatarURL, password: hashPass });
     res.status(201).json({
       status: "success",
       message: "user registered",
@@ -15,7 +17,7 @@ const register = async (req, res) => {
       data: {
         user: {
           email,
-          name,
+          avatarURL,
         },
       },
     });
