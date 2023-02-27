68bd315114914a609b6e3361f4fbf5e7d4ff7996
Albertukraine <101990726+Albertukraine@users.noreply.github.com>
2023-02-27 22:53:24 +0200

    add resize with jimp

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -1,27 +1,27 @@
-const User = require('../../models/user');
-const path = require('path');
-const fs = require('fs/promises');
+const User = require("../../models/user");
+const path = require("path");
+const fs = require("fs/promises");
+const jimp = require("jimp");
 
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 
-
-
 const updateAvatar = async (req, res) => {
-    const {path: tempUpload, originalname} = req.file;
-    const {_id: id} = req.user;
-    const avatarName = `${id}_${originalname}`;
-    try {
-        
-        const resultUpload = path.join(avatarsDir, avatarName);
-        await fs.rename(tempUpload, resultUpload);
-        const avatarURL = path.join("public", "avatars", avatarName);
-        await User.findByIdAndUpdate(req.user._id, {avatarURL});
-        res.json({avatarURL});
-        
-    } catch (error) {
-        await fs.unlink(tempUpload);
-        throw error;
-    }
-}
+  const { path: tempUpload, originalname } = req.file;
+  const { _id: id } = req.user;
+  const avatarName = `${id}_${originalname}`;
+  try {
+    const image = await jimp.read(tempUpload);
+    image.resize(250, 250).write(tempUpload);
+
+    const resultUpload = path.join(avatarsDir, avatarName);
+    await fs.rename(tempUpload, resultUpload);
+    const avatarURL = path.join("public", "avatars", avatarName);
+    await User.findByIdAndUpdate(req.user._id, { avatarURL });
+    res.json({ avatarURL });
+  } catch (error) {
+    await fs.unlink(tempUpload);
+    throw error;
+  }
+};
 
 module.exports = updateAvatar;
